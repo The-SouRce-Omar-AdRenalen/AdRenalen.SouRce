17da6d102f07594b4ab65cc91aae7ffad70fd2c2
Omaradrenalen <122954607+Omaradrenalen@users.noreply.github.com>
2024-03-19 03:45:33 +0200

    Update config.py

--- config.py
+++ config.py
@@ -24,6 +24,8 @@ LOGGER_ID = int(getenv("LOGGER_ID", None))
 # Get this value from @FallenxBot on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", 1356469075))
 
+AdRenalen.KleCha.play = "https://telegra.ph/file/6b073b212869b5630968f.jpg"
+
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
@@ -39,8 +41,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/WA_AdRenalen")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DEV_AdRenalen")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
