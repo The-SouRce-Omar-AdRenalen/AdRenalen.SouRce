767d445ea7e72e930f8d6f9ed9db854024709477
Omaradrenalen <122954607+Omaradrenalen@users.noreply.github.com>
2024-03-19 04:16:13 +0200

    Update config.py

--- config.py
+++ config.py
@@ -34,7 +34,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/AnonymousX1025/AdRenalen",
+    "https://github.com/Omaradrenalen/AdRenalen.SouRce",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
