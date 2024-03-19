cfdd7298c309d36ec52c7a6f60d81dba818232fc
Omaradrenalen <122954607+Omaradrenalen@users.noreply.github.com>
2024-03-19 13:08:30 +0200

    Update watcher.py

--- AdRenalen/plugins/misc/watcher.py
+++ AdRenalen/plugins/misc/watcher.py
@@ -2,7 +2,7 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from AdRenalen import app
-from AdRenalen.core.call import Anony
+from AdRenalen.core.call import Omar
 
 welcome = 20
 close = 30
@@ -11,4 +11,4 @@ close = 30
 @app.on_message(filters.video_chat_started, group=welcome)
 @app.on_message(filters.video_chat_ended, group=close)
 async def welcome(_, message: Message):
-    await Anony.stop_stream_force(message.chat.id)
+    await Omar.stop_stream_force(message.chat.id)
