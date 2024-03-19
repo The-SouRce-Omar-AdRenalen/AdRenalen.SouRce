ec17c7211a0b1d2244504770d1847f8f010a3f19
Omaradrenalen <122954607+Omaradrenalen@users.noreply.github.com>
2024-03-19 05:27:36 +0200

    Update stream.py

--- AdRenalen/utils/stream/stream.py
+++ AdRenalen/utils/stream/stream.py
@@ -6,12 +6,12 @@ from pyrogram.types import InlineKeyboardMarkup
 
 import config
 from AdRenalen import Carbon, YouTube, app
-from AdRenalen.core.call import Anony
+from AdRenalen.core.call import Omar
 from AdRenalen.misc import db
 from AdRenalen.utils.database import add_active_video_chat, is_active_chat
 from AdRenalen.utils.exceptions import AssistantErr
 from AdRenalen.utils.inline import aq_markup, close_markup, stream_markup
-from AdRenalen.utils.pastebin import AnonyBin
+from AdRenalen.utils.pastebin import OmarBin
 from AdRenalen.utils.stream.queue import put_queue, put_queue_index
 from AdRenalen.utils.thumbnails import get_thumb
 
@@ -32,7 +32,7 @@ async def stream(
     if not result:
         return
     if forceplay:
-        await Anony.force_stop_stream(chat_id)
+        await Omar.force_stop_stream(chat_id)
     if streamtype == "playlist":
         msg = f"{_['play_19']}\n\n"
         count = 0
@@ -79,7 +79,7 @@ async def stream(
                     )
                 except:
                     raise AssistantErr(_["play_14"])
-                await Anony.join_call(
+                await Omar.join_call(
                     chat_id,
                     original_chat_id,
                     file_path,
@@ -116,7 +116,7 @@ async def stream(
         if count == 0:
             return
         else:
-            link = await AnonyBin(msg)
+            link = await OmarBin(msg)
             lines = msg.count("\n")
             if lines >= 17:
                 car = os.linesep.join(msg.split(os.linesep)[:17])
@@ -165,7 +165,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await Anony.join_call(
+            await Omar.join_call(
                 chat_id,
                 original_chat_id,
                 file_path,
@@ -225,7 +225,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await Anony.join_call(chat_id, original_chat_id, file_path, video=None)
+            await Omar.join_call(chat_id, original_chat_id, file_path, video=None)
             await put_queue(
                 chat_id,
                 original_chat_id,
@@ -277,7 +277,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await Anony.join_call(chat_id, original_chat_id, file_path, video=status)
+            await Omar.join_call(chat_id, original_chat_id, file_path, video=status)
             await put_queue(
                 chat_id,
                 original_chat_id,
@@ -333,7 +333,7 @@ async def stream(
             n, file_path = await YouTube.video(link)
             if n == 0:
                 raise AssistantErr(_["str_3"])
-            await Anony.join_call(
+            await Omar.join_call(
                 chat_id,
                 original_chat_id,
                 file_path,
@@ -391,7 +391,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await Anony.join_call(
+            await Omar.join_call(
                 chat_id,
                 original_chat_id,
                 link,
