753c4ef8c29bc0d02c66aa0eef5dec4c4502c73c
Omaradrenalen <122954607+Omaradrenalen@users.noreply.github.com>
2024-03-19 03:36:43 +0200

    Add files via upload

---
+++ AdRenalen/__init__.py
@@ -0,0 +1,26 @@
+from AdRenalen.core.bot import Anony
+from AdRenalen.core.dir import dirr
+from AdRenalen.core.git import git
+from AdRenalen.core.userbot import Userbot
+from AdRenalen.misc import dbb, heroku
+
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+
+app = Anony()
+userbot = Userbot()
+
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
