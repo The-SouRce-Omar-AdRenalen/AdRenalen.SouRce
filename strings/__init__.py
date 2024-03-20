db362ba7ee4c473d49597d36f3ebc53be5ad9fde
Omaradrenalen <122954607+Omaradrenalen@users.noreply.github.com>
2024-03-20 15:50:21 +0200

    Update __init__.py

--- strings/__init__.py
+++ strings/__init__.py
@@ -1,3 +1,12 @@
+#
+# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
+#
+# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
+
 import os
 from typing import List
 
@@ -26,7 +35,7 @@ for filename in os.listdir(r"./strings"):
 for filename in os.listdir(r"./strings/langs/"):
     if "ar" not in languages:
         languages["ar"] = yaml.safe_load(
-            open(r"./strings/langs/en.yml", encoding="utf8")
+            open(r"./strings/langs/ar.yml", encoding="utf8")
         )
     if filename.endswith(".yml"):
         language_name = filename[:-4]
