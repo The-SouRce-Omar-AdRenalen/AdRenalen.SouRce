01f7e5a1ed27f3f5c21cd8eed6e78e4997b08468
Omaradrenalen <122954607+Omaradrenalen@users.noreply.github.com>
2024-03-20 13:53:20 +0200

    Update تعين اسم البوت.py

--- AdRenalen/plugins/play/تعين اسم البوت.py
+++ AdRenalen/plugins/play/تعين اسم البوت.py
@@ -40,8 +40,6 @@ async def caesar_bot(client, message):
     
     await message.reply_text(
         text=f"{bar}",
-        message.reply_photo(
-        photo=f"https://telegra.ph/file/6b073b212869b5630968f.jpg",
         disable_web_page_preview=True,
         reply_markup=keyboard
     )
