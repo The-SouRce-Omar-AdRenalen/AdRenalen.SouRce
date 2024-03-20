459354107ef112650eb8728f75ae6efb4ae46514
Omaradrenalen <122954607+Omaradrenalen@users.noreply.github.com>
2024-03-20 05:03:55 +0200

    Update السورس.py

--- AdRenalen/plugins/play/السورس.py
+++ AdRenalen/plugins/play/السورس.py
@@ -14,7 +14,6 @@ from pyrogram.enums import ChatMemberStatus
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
 from AdRenalen import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
 from AdRenalen import app
-from telegraph import upload_file
 from asyncio import gather
 from pyrogram.errors import FloodWait
 ##############################################################
