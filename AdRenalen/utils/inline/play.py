f9545fbc87090b73800e659b2c974d04a65f163a
Omaradrenalen <122954607+Omaradrenalen@users.noreply.github.com>
2024-03-21 06:20:02 +0200

    Update play.py

--- AdRenalen/utils/inline/play.py
+++ AdRenalen/utils/inline/play.py
@@ -50,7 +50,7 @@ def stream_markup_timer(_, chat_id, played, dur):
         bar = "———————◉——"
     elif 80 <= umm < 95:
         bar = "————————◉—"
-    else:
+    else
         bar = "—————————◉"
     buttons = [
         def stream_markup_timer(_, chat_id, played, dur):
