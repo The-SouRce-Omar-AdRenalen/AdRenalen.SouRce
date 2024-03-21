6fa1d9d6897bfac7c73f5f5923e422c52c015d0a
Omaradrenalen <122954607+Omaradrenalen@users.noreply.github.com>
2024-03-22 00:42:26 +0200

    Update المالك.py

--- AdRenalen/plugins/play/المالك.py
+++ AdRenalen/plugins/play/المالك.py
@@ -1,43 +1,8 @@
 import asyncio
 import os
-import time
-import requests
-from pyrogram import enums
-import aiohttp
 from pyrogram import filters
 from pyrogram import Client
-from pyrogram.enums import ChatMemberStatus
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
-from AdRenalen import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
 from AdRenalen import app
-from telegraph import upload_file
-from asyncio import gather
-from pyrogram.errors import FloodWait
-
-
-AdRenaleno = []
-@app.on_message(filters.command(["المالك"], ""), group=999)
-async def ownner(client: Client, message: Message):
-    async for m in app.get_chat_members(message.chat.id, filter=enums.ChatMembersFilter.ADMINISTRATORS):
-         if m.status == ChatMemberStatus.OWNER:
-            AdRenaleno.append(m.user.id)
-    if len(AdRenaleno) != 0:        
-       m = await app.get_users(int(AdRenaleno[0]))
-       if m.photo:
-         async for photo in app.get_chat_photos(AdRenaleno[0],limit=1):
-          await message.reply_photo(photo.file_id,caption=f"**⤄الاسم: {message.from_user.mention}\n⤄اليوزر: @{message.from_user.username}\n⤄ايدي:`{message.from_user.id}`\nʙɪᴏᚐ: {usr.bio}\n⤄جروب: {message.chat.title}\n⤄ايدي الجروب : `{message.chat.id}`**",reply_markup=InlineKeyboardMarkup(
-             [              
-               [          
-                 InlineKeyboardButton(m.first_name, url=f"https://t.me/{m.username}")
-               ],             
-             ]                 
-            )                     
-          )
-       else:
-        await message.reply_text(f"**⤄الاسم: {message.from_user.mention}\n⤄اليوزر: @{message.from_user.username}\n⤄ايدي:`{message.from_user.id}`\nʙɪᴏᚐ: {usr.bio}\n⤄جروب: {message.chat.title}\n⤄ايدي الجروب : `{message.chat.id}`**", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(m.first_name, url=f"https://t.me/{m.username}")],]))
-    else:
-        await message.reply_text("الاك محذوف يقلب")
-
 
 
 @app.on_message(filters.command(["اسمي","اسمي اي"], ""), group=123222)
