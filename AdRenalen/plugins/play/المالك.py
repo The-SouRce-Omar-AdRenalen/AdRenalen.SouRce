5829c378f422453c591726906ffc72d51ae0f8f3
Omaradrenalen <122954607+Omaradrenalen@users.noreply.github.com>
2024-03-22 00:23:11 +0200

    Update المالك.py

--- AdRenalen/plugins/play/المالك.py
+++ AdRenalen/plugins/play/المالك.py
@@ -16,15 +16,15 @@ from pyrogram.errors import FloodWait
 
 
 AdRenaleno = []
-@app.on_message(filters.command(["المالك", "صاحب الخرابه", "المنشي"], ""), group=222)
+@app.on_message(filters.command(["المالك"], ""), group=999)
 async def ownner(client: Client, message: Message):
     async for m in app.get_chat_members(message.chat.id, filter=enums.ChatMembersFilter.ADMINISTRATORS):
          if m.status == ChatMemberStatus.OWNER:
             AdRenaleno.append(m.user.id)
     if len(AdRenaleno) != 0:        
-       m = await app.get_users(int(x[0]))
+       m = await app.get_users(int(AdRenaleno[0]))
        if m.photo:
-         async for photo in app.get_chat_photos(x[0],limit=1):
+         async for photo in app.get_chat_photos(AdRenaleno[0],limit=1):
           await message.reply_photo(photo.file_id,caption=f"**⤄الاسم: {message.from_user.mention}\n⤄اليوزر: @{message.from_user.username}\n⤄ايدي:`{message.from_user.id}`\nʙɪᴏᚐ: {usr.bio}\n⤄جروب: {message.chat.title}\n⤄ايدي الجروب : `{message.chat.id}`**",reply_markup=InlineKeyboardMarkup(
              [              
                [          
@@ -40,7 +40,7 @@ async def ownner(client: Client, message: Message):
 
 
 
-@app.on_message(filters.command(["اسمي"], ""), group=123222)
+@app.on_message(filters.command(["اسمي","اسمي اي], ""), group=123222)
 async def vgdg(client: Client, message: Message):
     await message.reply_text(
-        f"""🥀❄️ اسمك »»  `{message.from_user.mention}`""") 
+        f"""- اسمك » ⦗ {message.from_user.mention} ⦘ 💘 ⋅""") 
