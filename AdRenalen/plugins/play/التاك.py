d9c40f0a0345cceae2255e61dc5d8dff4eaa71bf
Omaradrenalen <122954607+Omaradrenalen@users.noreply.github.com>
2024-03-21 05:06:09 +0200

    Update التاك.py

--- AdRenalen/plugins/play/التاك.py
+++ AdRenalen/plugins/play/التاك.py
@@ -61,7 +61,7 @@ async def nummmm(client: app, message):
   array.remove(message.chat.id)
 
 
-@app.on_message(filters.command(["ايقاف المنشن","تعطيل المنشن","/cancel", "ايقاف التاك"] ""), group=822)
+@app.on_message(filters.command(["ايقاف المنشن","تعطيل المنشن","/cancel", "ايقاف التاك"], ""), group=822)
 async def stop(client, message):
   chek = await client.get_chat_member(message.chat.id, message.from_user.id)
   if not chek.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
