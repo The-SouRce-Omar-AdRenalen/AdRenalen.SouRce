cd06b2eca59ce06b4c092bf10e78206f9f0e87cf
Omaradrenalen <122954607+Omaradrenalen@users.noreply.github.com>
2024-03-22 03:52:53 +0200

    Update همسه.py

--- AdRenalen/plugins/play/همسه.py
+++ AdRenalen/plugins/play/همسه.py
@@ -53,13 +53,14 @@ async def send_hms(client, message):
             [
                     InlineKeyboardButton("‹ مستلم الهمسه 💘 ›", callback_data="{tg://openmessage?user_id={to_id}"),
                 ],[
-                    InlineKeyboardButton("‹ مرسل الهمسه 💘 ›", callback_data="{tg://openmessage?user_id={from_id}"), 
+                    InlineKeyboardButton("‹ مرسل الهمسه 💘 ›", url=f"{from_url}"), 
                     InlineKeyboardButton("‹ اظغط لرؤية الهمسه 💘 ›", callback_data="hms_answer"),
                 ],[
                     InlineKeyboardButton(
                         "‹ 𝐂𝐇.𝐒𝐎𝐔𝐑𝐂𝐄 ›", url=f"http://t.me/WA_ADRENALEN"),
             ]
-        ]
+        ]                   
+
          ),
      )
         
