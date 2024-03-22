2293f434ea978e3dfaf97808f60cba9639077ff4
Omaradrenalen <122954607+Omaradrenalen@users.noreply.github.com>
2024-03-22 13:25:55 +0200

    Update تفعيل.py

--- AdRenalen/plugins/admins/تفعيل.py
+++ AdRenalen/plugins/admins/تفعيل.py
@@ -7,8 +7,7 @@ from pyrogram import Client, filters
 DEVELOPERS = [1924832439]
 OWNER_BOT = 1924832439
 
-
-@app.on_message(filters.command("تفعيل", ""))
+@app.on_message(filters.command(["تفعيل"], ""), group=221312)
 def update_owners(client, message):
     chat_id = str(message.chat.id)
     Toom = message.from_user
