e3043d88d67a4f39b5a5e1b0cd9033c000875df2
The-SouRce-Omar-AdRenalen <122954607+The-SouRce-Omar-AdRenalen@users.noreply.github.com>
2024-03-25 23:37:53 +0200

    Update بوت.py

--- AdRenalen/plugins/play/بوت.py
+++ AdRenalen/plugins/play/بوت.py
@@ -41,5 +41,5 @@ async def caesar_bot(client, message):
     await message.reply_text(
         text=f"**[{bar}](https://t.me/{bot_username}?startgroup=True)**",
         disable_web_page_preview=True,
-        reply_markup=keyboard
-    ),parse_mode=enums.ParseMode.MARKDOWN)
+        reply_markup=keyboard,
+    parse_mode=enums.ParseMode.MARKDOWN)
