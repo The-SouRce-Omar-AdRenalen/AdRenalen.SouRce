9c75b62ec495c2b9d803c85ade73d87e1d3d892f
The-SouRce-Omar-AdRenalen <122954607+The-SouRce-Omar-AdRenalen@users.noreply.github.com>
2024-03-25 18:05:18 +0200

    Update thumbnails.py

--- AdRenalen/utils/thumbnails.py
+++ AdRenalen/utils/thumbnails.py
@@ -87,7 +87,7 @@ async def get_thumb(videoid):
             background = enhancer.enhance(0.6)
             image2 = background
 
-            circle = Image.open config import YOUTUBE_IMG_URL
+            circle = "https://telegra.ph/file/a982187bd0641ac430c78.jpg")
 
             # changing circle color
             im = circle
