3ff6c31b3164246bb14bc556f07051cb3c876206
The-SouRce-Omar-AdRenalen <122954607+The-SouRce-Omar-AdRenalen@users.noreply.github.com>
2024-03-25 21:46:29 +0200

    Update thumbnails.py

--- AdRenalen/utils/thumbnails.py
+++ AdRenalen/utils/thumbnails.py
@@ -86,7 +86,7 @@ async def get_thumb(videoid):
             background = enhancer.enhance(0.6)
             image2 = background
 
-            circle = Image.open("AdRenalen/assets/AdRenalen.mp4")
+            circle = Image.open("AdRenalen/assets/Omar.png")
 
             # changing circle color
             im = circle
