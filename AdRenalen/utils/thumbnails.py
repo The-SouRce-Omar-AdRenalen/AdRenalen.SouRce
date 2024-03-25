ad4d3a3b7a8e9bb8fc635d159ddc9afc9a986778
The-SouRce-Omar-AdRenalen <122954607+The-SouRce-Omar-AdRenalen@users.noreply.github.com>
2024-03-25 04:25:05 +0200

    Update thumbnails.py

--- AdRenalen/utils/thumbnails.py
+++ AdRenalen/utils/thumbnails.py
@@ -125,7 +125,7 @@ async def get_thumb(videoid):
 
             image4 = ImageDraw.Draw(image2)
             image4.text(
-                (5, 5), "SourceToxiC ", fill="white", font=font1, align="center"
+                (10, 10), "SourceToxiC ", fill="white", font=font1, align="left"
             )
             image4.text(
                 (670, 150),
@@ -163,12 +163,12 @@ async def get_thumb(videoid):
             duration = f"Duration : {duration} Mins"
             channel = f"Channel : @SourceToxiC"
 
-            image4.text((670, 450), text=views, fill="white", font=font4, align="center")
+            image4.text((670, 450), text=views, fill="white", font=font4, align="left")
             image4.text(
-                (670, 500), text=duration, fill="white", font=font4, align="center"
+                (670, 500), text=duration, fill="white", font=font4, align="left"
             )
             image4.text(
-                (670, 550), text=channel, fill="white", font=font4, align="center"
+                (670, 550), text=channel, fill="white", font=font4, align="left"
             )
 
             image2 = ImageOps.expand(image2, border=0, fill=make_col())
