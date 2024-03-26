cf9b86193be67ef1e75677d08fed64000f1b5242
The-SouRce-Omar-AdRenalen <122954607+The-SouRce-Omar-AdRenalen@users.noreply.github.com>
2024-03-26 20:34:04 +0200

    Update همسه.py

--- AdRenalen/plugins/play/همسه.py
+++ AdRenalen/plugins/play/همسه.py
@@ -49,7 +49,7 @@ async def send_hms(client, message):
         
         await app.send_message(
     chat_id=in_id,
-    text=f"**{(await app.get_chat(to_id)).first_name}](tg://openmessage?user_id={to_id})\n{(await app.get_chat(from_id)).first_name}](tg://openmessage?user_id={from_id})\nهمسه جديدة استعمل الازرار لرؤية الهمسه 💘 ⋅**" 
+    text=f"[{app.get_chat.first_name}](tg://openmessage?user_id={to_id}استعمل الازرار لرؤية الهمسه 💘 ⋅**" 
     reply_markup=InlineKeyboardMarkup(
         [
             [
