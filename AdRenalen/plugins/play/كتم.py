73da57722e80235220ae795add58269fbe982f75
The-SouRce-Omar-AdRenalen <122954607+The-SouRce-Omar-AdRenalen@users.noreply.github.com>
2024-03-27 17:51:50 +0200

    Update كتم.py

--- AdRenalen/plugins/play/كتم.py
+++ AdRenalen/plugins/play/كتم.py
@@ -12,51 +12,51 @@ mutes = []
 async def mute(app,message):
    member = await message.chat.get_member(message.from_user.id)
    if not member.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
-     return await message.reply("**Only chat admins can use this command**")
+     return await message.reply("- يجب ان تكون مشرفا لكتم الاعضاء 💘 ⋅")
    else:
      if not message.reply_to_message:
-       return await message.reply("**• You must reply to an user first**")
+       return await message.reply("- عليك الرد علي مستخدم وان يكون عضو ليس مشرف 💘 ⋅")
      member = await message.chat.get_member(message.reply_to_message.from_user.id)
      if member.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
-       return await message.reply("**You can't mute chat admin**")
+       return await message.reply("- لا يمكنني كتم المشرفين 💘 ⋅")
      chat_id = str(message.chat.id)
      user_id = str(message.reply_to_message.from_user.id)
      x = "{}@{}".format(chat_id,user_id)
      if x in mutes:
-       return await message.reply("**This user already muted in this chat**")
+       return await message.reply("- تم كتم هذا العضو من قبل 💘 ⋅")
      else:
        mutes.append(x)
-       return await message.reply("**{} has muted successfully by {} .**".format(message.reply_to_message.from_user.mention,message.from_user.mention))
+       return await message.reply("- العضو ⦗ {} ⦘  تم كتمة بواسطة ⦗ {} ⦘ 💘 ⋅".format(message.reply_to_message.from_user.mention,message.from_user.mention))
        
 @app.on_message(filters.command(["الغاء الكتم"],"") & filters.group)
 async def unmute(app,message):
    member = await message.chat.get_member(message.from_user.id)
    if not member.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
-     return await message.reply("**Only chat admins can use this command**")
+     return await message.reply("- هذا الامر للمشرفين فقط 💘 ⋅")
    else:
      if not message.reply_to_message:
-       return await message.reply("**• You must reply to an user first**")
+       return await message.reply("- عليك الرد علي مستخدم وان يكون عضو ليس مشرف 💘 ⋅")
      member = await message.chat.get_member(message.reply_to_message.from_user.id)
      if member.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
-       return await message.reply("**You can't unmute chat admin**")
+       return await message.reply("- لا يمكنني استعمال الامر علي المشرفين 💘 ⋅")
      chat_id = str(message.chat.id)
      user_id = str(message.reply_to_message.from_user.id)
      x = "{}@{}".format(chat_id,user_id)
      if not x in mutes:
-       return await message.reply("**This user already unmuted in this chat**")
+       return await message.reply("- تم الغاء كتم المستخدم في هذه المجموعة بنجاح 💘 ⋅)
      else:
        mutes.remove(x)
-       return await message.reply("**{} has unmuted successfully by {} .**".format(message.reply_to_message.from_user.mention,message.from_user.mention))
+       return await message.reply("- العضو ⦗ {} ⦘  تم الغاء كتمة بواسطة ⦗ {} ⦘ 💘 ⋅".format(message.reply_to_message.from_user.mention,message.from_user.mention))
 
 @app.on_message(filters.command(["المكتومين"],"")   & filters.group)
 def get_dmute(app, message):
    if len(mutes) == 0: return
    member = message.chat.get_member(message.from_user.id)
    if not member.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
-     return message.reply("**Only chat admins can use this command**")
+     return message.reply("- هذا الامر للمشرفين فقط 💘 ⋅")
    ch = message.chat.id
    c = 0
-   text = "• Mutes list in this chat :\n\n"
+   text = "- المكتومين في هذه الـ مجموعة : \n"
    for a in mutes:
      chat_id = int(a.split("@")[0])
      user_id = int(a.split("@")[1])
@@ -64,8 +64,8 @@ def get_dmute(app, message):
         user = app.get_users(user_id)
         text += f"- {user.mention}\n"
         c += 1
-   if c == 0: return message.reply("**No one muted in this chat**")
-   message.reply(f"**{text}**")
+   if c == 0: return message.reply("- لا يوجد مكتومين في هذه المجموعة 💘 ⋅")
+   message.reply(f"{text}")
 
 @app.on_message(filters.group)
 def del_msg(_,m):
