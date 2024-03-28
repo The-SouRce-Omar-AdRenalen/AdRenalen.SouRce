296ad50b6664ba288c88a307d69352cec094ddd2
The-SouRce-Omar-AdRenalen <122954607+The-SouRce-Omar-AdRenalen@users.noreply.github.com>
2024-03-28 15:28:14 +0200

    Update رد متعدد.py

--- AdRenalen/plugins/play/رد متعدد.py
+++ AdRenalen/plugins/play/رد متعدد.py
@@ -7,7 +7,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 Omar = ["صلي علي النبي وتبسم", "بتكلمني انا", "معرفش🤔", "اللي ضاع من عمرو سنين نيهه😹", "انت متعرفنيش بجد اخص علي الصحب؟!"]
 
-@app.on_message(filters.text & ~filters.edited & filters.regex([".","..","..."],"")
+@app.on_message(filters.text & ~filters.edited & filters.regex(r"(^|.|$)"))
 async def Omarmusic(client, message):
     if "مين" in message.text:
         response = random.choice(Omar)
